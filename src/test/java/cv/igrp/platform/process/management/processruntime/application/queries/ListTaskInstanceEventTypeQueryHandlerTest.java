288fac11f19556e2a9ac11c9c9e6f7a32d680c02
venceslau.gama <venceslau.gama@nosi.cv>
2025-08-02 12:41:36 -0100

    config params query

---
+++ src/test/java/cv/igrp/platform/process/management/processruntime/application/queries/ListTaskInstanceEventTypeQueryHandlerTest.java
@@ -0,0 +1,35 @@
+package cv.igrp.platform.process.management.processruntime.application.queries;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ListTaskInstanceEventTypeQueryHandlerTest {
+
+  @InjectMocks
+  private ListTaskInstanceEventTypeQueryHandler listTaskInstanceEventTypeQueryHandler;
+
+  @BeforeEach
+  void setUp() {
+    // TODO: Initialize mock dependencies if needed
+  }
+
+  @Test
+  void testHandleListTaskInstanceEventTypeQuery() {
+    // TODO: Implement unit test for handle method
+    // Example:
+    // Given
+    // ListTaskInstanceEventTypeQuery query = new ListTaskInstanceEventTypeQuery(...);
+    //
+    // When
+    // ResponseEntity<List<ConfigParameterDTO>> response = listTaskInstanceEventTypeQueryHandler.handle(query);
+    //
+    // Then
+    // assertNotNull(response);
+    // assertEquals(..., response.getBody());
+  }
+
+}
---
+++ src/test/java/cv/igrp/platform/process/management/processruntime/application/queries/ListTaskInstanceStatusQueryHandlerTest.java
@@ -0,0 +1,35 @@
+package cv.igrp.platform.process.management.processruntime.application.queries;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ListTaskInstanceStatusQueryHandlerTest {
+
+  @InjectMocks
+  private ListTaskInstanceStatusQueryHandler listTaskInstanceStatusQueryHandler;
+
+  @BeforeEach
+  void setUp() {
+    // TODO: Initialize mock dependencies if needed
+  }
+
+  @Test
+  void testHandleListTaskInstanceStatusQuery() {
+    // TODO: Implement unit test for handle method
+    // Example:
+    // Given
+    // ListTaskInstanceStatusQuery query = new ListTaskInstanceStatusQuery(...);
+    //
+    // When
+    // ResponseEntity<String> response = listTaskInstanceStatusQueryHandler.handle(query);
+    //
+    // Then
+    // assertNotNull(response);
+    // assertEquals(..., response.getBody());
+  }
+
+}
