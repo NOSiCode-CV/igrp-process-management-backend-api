bd0959707b0ff3610f1ae0a89ef683438afd524a
Ivanick.Santos <ivanick.santos@gov.cv>
2025-08-07 15:15:18 -0100

    refactor(process-management): add `PENDING` status to `TaskInstanceStatus` enum and JSON mapping

--- src/main/java/cv/igrp/platform/process/management/shared/application/constants/TaskInstanceStatus.java
+++ src/main/java/cv/igrp/platform/process/management/shared/application/constants/TaskInstanceStatus.java
@@ -14,12 +14,12 @@ import java.util.stream.Collectors;
 public enum TaskInstanceStatus implements IgrpEnum<String> {
 
   CREATED("CREATED", "Criado"),
-    ASSIGNED("ASSIGNED", "Atribuido"),
-    SUSPENDED("SUSPENDED", "Suspenso"),
-    COMPLETED("COMPLETED", "Completo"),
-    CANCELLED("CANCELLED", "Cancelado"),
-    DELETED("DELETED", "Eliminado")
-  ;
+  ASSIGNED("ASSIGNED", "Atribuido"),
+  SUSPENDED("SUSPENDED", "Suspenso"),
+  COMPLETED("COMPLETED", "Completo"),
+  CANCELLED("CANCELLED", "Cancelado"),
+  DELETED("DELETED", "Eliminado"),
+  PENDING("PENDING", "Pendente");
 
   private final String code;
   private final String description;
@@ -40,33 +40,35 @@ public enum TaskInstanceStatus implements IgrpEnum<String> {
   }
 
   /**
-  * Pre-built maps for fast lookup.
-  */
+   * Pre-built maps for fast lookup.
+   */
   private static final Map<String, TaskInstanceStatus> CODE_MAP = Arrays.stream(values())
-          .collect(Collectors.toMap(TaskInstanceStatus::getCode, Function.identity()));
+      .collect(Collectors.toMap(TaskInstanceStatus::getCode, Function.identity()));
 
   /**
-  * Attempts to find the enum value associated with the given code.
-  * @param code The code to look up
-  * @return An Optional containing the enum value if found, empty Optional otherwise
-  */
+   * Attempts to find the enum value associated with the given code.
+   *
+   * @param code The code to look up
+   * @return An Optional containing the enum value if found, empty Optional otherwise
+   */
   public static Optional<TaskInstanceStatus> fromCode(String code) {
     return Optional.ofNullable(CODE_MAP.get(code));
   }
 
   /**
-  * Finds the enum value associated with the given code or throws an exception if not found.
-  * @param code The code to look up
-  * @return The enum value for the given code
-  * @throws IllegalArgumentException if no enum value exists for the given code
-  */
+   * Finds the enum value associated with the given code or throws an exception if not found.
+   *
+   * @param code The code to look up
+   * @return The enum value for the given code
+   * @throws IllegalArgumentException if no enum value exists for the given code
+   */
   public static TaskInstanceStatus fromCodeOrThrow(String code) {
     return fromCode(code).orElseThrow(() -> new IllegalArgumentException("Invalid TaskInstanceStatus for this code: " + code));
   }
 
   /**
-  * Returns a map of code to description.
-  */
+   * Returns a map of code to description.
+   */
   public static Map<String, String> codeDescriptionMap() {
     return CODE_MAP.values().stream().collect(Collectors.toMap(TaskInstanceStatus::getCode, TaskInstanceStatus::getDescription));
   }
