04fa9a3a394dc7146c089f197dae0d60c984305d
venceslau.gama <venceslau.gama@nosi.cv>
2025-08-08 18:42:36 -0100

    list task event type query handler test

--- src/test/java/cv/igrp/platform/process/management/processruntime/application/queries/GetTaskInstanceByIdQueryHandlerTest.java
+++ src/test/java/cv/igrp/platform/process/management/processruntime/application/queries/GetTaskInstanceByIdQueryHandlerTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class GetTaskInstanceByIdQueryHandlerTest {
+class GetTaskInstanceByIdQueryHandlerTest {
 
   @Mock
   private TaskInstanceService taskInstanceService;
