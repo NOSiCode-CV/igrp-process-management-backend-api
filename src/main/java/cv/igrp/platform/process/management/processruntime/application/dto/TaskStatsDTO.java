c0c577b50cc6a1381ec7a483b0f4e86d3952ffb6
venceslau.gama <venceslau.gama@nosi.cv>
2025-08-27 15:40:03 -0100

    task stats dto

---
+++ src/main/java/cv/igrp/platform/process/management/processruntime/application/dto/TaskStatsDTO.java
@@ -0,0 +1,33 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.igrp.platform.process.management.processruntime.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class TaskStatsDTO  {
+
+
+
+  private Long totalTaskInstances ;
+
+
+  private Long totalAvailableTasks ;
+
+
+  private Long totalCompletedTasks ;
+
+
+  private Long totalCanceledTasks ;
+
+}
