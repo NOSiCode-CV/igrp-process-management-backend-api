0f95353d3b869f339d0e68cc63205d973cedf683
venceslau.gama <venceslau.gama@nosi.cv>
2025-09-03 15:08:01 -0100

    fix bug

--- src/main/java/cv/igrp/platform/process/management/processruntime/mappers/ProcessInstanceMapper.java
+++ src/main/java/cv/igrp/platform/process/management/processruntime/mappers/ProcessInstanceMapper.java
@@ -65,7 +65,7 @@ public class ProcessInstanceMapper {
         .canceledBy(processInstanceEntity.getCanceledBy())
         .obsCancel(processInstanceEntity.getObsCancel())
         .applicationBase(Code.create(processInstanceEntity.getApplicationBase()))
-
+        .priority(processInstanceEntity.getPriority())
         .build();
   }
 
