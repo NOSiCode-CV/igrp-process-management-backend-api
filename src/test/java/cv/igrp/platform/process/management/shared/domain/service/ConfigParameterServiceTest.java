94e23673aeb8ea5ba3358b6cf803253e3bb9f785
venceslau.gama <venceslau.gama@nosi.cv>
2025-09-08 11:19:35 -0100

    fix error in test

--- src/test/java/cv/igrp/platform/process/management/shared/domain/service/ConfigParameterServiceTest.java
+++ src/test/java/cv/igrp/platform/process/management/shared/domain/service/ConfigParameterServiceTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConfigParameterServiceTest {
+class ConfigParameterServiceTest {
 
   private ConfigParameterService service;
 
