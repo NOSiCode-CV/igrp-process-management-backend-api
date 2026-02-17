98b92e9d4f20a88cb285289f17992f236fc7cec4
iekini.fernandes <iekini.fernandes@nosi.cv>
2026-02-17 22:40:48 -0100

    Refactor `ActivitiesController` and related services to support `processIdentifier`:
    - Replace `processInstanceId` with `processIdentifier` across JSON schema, REST controllers, queries, and services.
    - Add support for `Business Key` lookup in `ActivityInstanceService`.
    - Update dependencies to `0.1.0-beta.19` for `runtime-core` and `runtime-activiti`.
    - Improve `GetActivityInstancesQuery` and `GetActivityProgressQuery` handlers to handle `processIdentifier`.

--- src/main/java/cv/igrp/platform/process/management/processruntime/application/queries/GetActivityProgressQueryHandler.java
+++ src/main/java/cv/igrp/platform/process/management/processruntime/application/queries/GetActivityProgressQueryHandler.java
@@ -1,7 +1,6 @@
 package cv.igrp.platform.process.management.processruntime.application.queries;
 
 import cv.igrp.framework.runtime.core.engine.activity.model.IGRPActivityType;
-import cv.igrp.platform.process.management.processruntime.domain.service.ActivityInstanceService;
 import cv.igrp.platform.process.management.processruntime.mappers.ActivityMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.UUID;
 
+import cv.igrp.platform.process.management.processruntime.domain.service.ActivityInstanceService;
 import cv.igrp.platform.process.management.processruntime.application.dto.ActivityProgressDTO;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,11 +33,18 @@ public class GetActivityProgressQueryHandler implements QueryHandler<GetActivity
   @Transactional
   @IgrpQueryHandler
   public ResponseEntity<List<ActivityProgressDTO>> handle(GetActivityProgressQuery query) {
-    return ResponseEntity.ok(activityMapper.toProgressesDto(
-        activityInstanceService.getProcessTimelineEvents(
-            UUID.fromString(query.getProcessInstanceId()),
-            query.getType() != null ? IGRPActivityType.valueOf(query.getType()) : null
-        )));
+    IGRPActivityType type = query.getType() != null
+        ? IGRPActivityType.valueOf(query.getType())
+        : null;
+    return ResponseEntity.ok(
+        activityMapper.toProgressesDto(
+          activityInstanceService.getProcessTimelineEvents(
+              query.getProcessIdentifier(),
+              type
+          )
+        )
+    );
+
   }
 
 }
